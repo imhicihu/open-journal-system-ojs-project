fb4f60adea40f3b310dd17339a67b033fe831b11
IMHICIHU <imhicihu@gmail.com>
2023-07-22 21:51:19 -0300

    uploads.php initial stage

---
+++ uploads.php
@@ -0,0 +1,12 @@
+if (move_uploaded_file($_FILES[$fileName]['tmp_name'], $destFileName)){    
+				
+    			$allowed 	=   array('gif','png' ,'jpg', 'pdf', 'doc', 'docx'); // Type Extension
+    			$filename 	=   $_FILES[$fileName]['name'];
+    			$ext 		=   strtolower(pathinfo($filename, PATHINFO_EXTENSION));    
+		
+    			if(!in_array($ext,$allowed) ) {
+    				return false;    				
+    			}else{    				
+    				return $this->setMode($destFileName, FILE_MODE_MASK);    				
+    			}   		    			
+}
